4b9da9fbfe573d5550fa27531c7cb79268935a5f
Myeongjae Seo <peterseo1004@icloud.com>
2026-07-30 22:49:49 -0500

    just added a few shizzle

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 import ReactMarkdown from "react-markdown";
 
 function App() {
 
   const markdowntext = `
-    # React Markdown Example
+## React Markdown Example
 
 - Some text
 - Some other text
@@ -33,11 +33,14 @@ This is a [link](https://github.com/remarkjs/react-markdown)
 
 
   const ChallengeEssay = `
-==The lessons we take from obstacles we encounter can be fundamental to later success. Recount a time when you faced a challenge, setback, or failure. How did it affect you, and what did you learn from the experience?
 
+## ==The lessons we take from obstacles we encounter can be fundamental to later success. 
+Recount a time when you faced a challenge, setback, or failure. 
+How did it affect you, and what did you learn 
+from the experience?
 
-I first faced a challenge in my sophomore year of my life. I was taking AP Physics 1, which was  a huge step off the status quo at my school. Our normal course plants are to take AP physics 1 at junior year, but I decided to take it a year before so that in the future, I can practice to take the AP Physics C courses on my own.
-Taking that course was, in a lot of ways, a mistake. I wasn't able to catch up with a lot of classmates, who, seemed to be having a breeze with all the coursework. my grades were getting lower and lower with every grade, my ego even lower. Never in my life, have I ever gotten a B in any class up to this point, and to me it felt as if there was no other way. I had no belief in myself, no trust about what I was good at, and in general i was extremely sad. However, that all changed with me figuring out what to do. I tried to stay in the other teacher, Mr. M's room after school up until 6 every day to fix and learn my material. Needless to say, I was amazed to find my grade of 100 on my final 
+I was able to learn the 
+
 
 
 `;
@@ -49,10 +52,13 @@ Taking that course was, in a lot of ways, a mistake. I wasn't able to catch up w
   return (
     <div className="App">
 
+      <title>JudasLilith</title>
 
 
       <div className="TabTitle">
         <title>essay blog</title>
+        <p><i>This is my rifle - this is my gun, this one's for fighting; this one's for fun!</i></p>
+
       </div>
 
       <div className="PageTitle">
@@ -65,10 +71,8 @@ Taking that course was, in a lot of ways, a mistake. I wasn't able to catch up w
         <section>
           <ReactMarkdown>{markdowntext}</ReactMarkdown>
         </section>
-        <p> nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn</p>
         <ReactMarkdown>{ChallengeEssay}</ReactMarkdown>
       </div>
-
 
 
 
@@ -82,17 +86,32 @@ Taking that course was, in a lot of ways, a mistake. I wasn't able to catch up w
 
 
 
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+
+
+
+
+
+      </header>
+
+      <div className="bottom">
+        <p>About me:</p>
+        <p> I am an Engineering Student from Texas. My hobbies are 3D printing, Vanilla Web Dev, Video game creation(Godot and Unity), CNC & milling, and much more:</p>
+        <li>Gardening(especially herbs)</li>
+        <li>Computer Designing(OnShape, FreeCad, Inventor, and Fusion360)</li>
+        <li>Blacksmithing(Ringmaking & knifemaking)</li>
+        <li>Linux Ricing(Hyprland)</li>
+        <li>Hobby CNCing</li>
+        <li>Hobby Machine Milling</li>
+        <li>ESP32, Arduinos, and Raspberry Pis</li>
+
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://judaslilith.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learngsdgbf React
-
+          my other website:
         </a>
 
 
@@ -101,7 +120,11 @@ Taking that course was, in a lot of ways, a mistake. I wasn't able to catch up w
 
 
 
-      </header>
+      </div>
+
+
+
+
     </div>
   );
 }
---
+++ text/Writing ideas.md
@@ -0,0 +1,2 @@
+* Try writing about Demian's introduction quote, talking about how the bird comes out of the egg
+
